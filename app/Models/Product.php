ca5e826062396797113e9e5604ba760520b438c6
Jawad-Ahmad786 <jawadahmad78692@gmail.com>
2024-07-31 23:17:28 +0500

    "Relationshps has been made in models"

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -8,4 +8,17 @@ use Illuminate\Database\Eloquent\Model;
 class Product extends Model
 {
     use HasFactory;
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+    public function brand()
+    {
+        return $this->belongsTo(Brand::class);
+    }
+    public function orderItems()
+    {
+        return $this->hasMany(OrderItem::class);
+    }
 }
