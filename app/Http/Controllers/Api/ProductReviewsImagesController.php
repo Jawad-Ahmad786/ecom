cd1a807b2106be79e28f3e445dcaef1e32d5c059
Aamish26622 <aamishirfan2662@gmail.com>
2024-09-03 15:09:45 +0500

    Product review images has been deleted

---
+++ app/Http/Controllers/Api/ProductReviewsImagesController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\ProductReview;
+use App\Services\ImagesService;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class ProductReviewsImagesController extends Controller
+{
+    protected ImagesService $imagesService;
+
+    public function __construct(ImagesService $imagesService){
+        $this->imagesService = $imagesService;
+    }
+
+    public function destroy(ProductReview $productReview, Request $request): JsonResponse
+    {
+       if(!(Auth::user()->id === $productReview->user_id)){
+            return response()->json([
+                'message' => 'You are not authorize to delete these images'
+            ], 403);
+       }
+            $images = json_decode($request->images, true);
+
+            $imagesDeleted = $this->imagesService->deleteImages($productReview, $images);
+            if(!$imagesDeleted){
+                return response()->json([
+                    'message' => 'An issue occurred while deleting images'
+                ]);
+            }
+            return response()->json([
+                'message' => 'Images deleted successfully'
+            ], 200);
+
+    }
+}
---
+++ app/Services/ImagesService.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Services;
+
+use App\Models\Product;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Storage;
+
+class ImagesService
+{
+    public function storeImages(Product $product, array $images): bool|string
+    {
+        try {
+          foreach ($images as $image) {
+              $path = $image->store('products');
+              $product->images()->create([
+                  'image' => $path
+              ]);
+          }
+          return true;
+      } catch(\Exception $e){
+           return "An error occured while uploading images.";
+      }
+    }
+    public function deleteImages(object $model, array $images= []): bool
+    {
+       try{
+           foreach($model->images as $image)
+           {
+             if(!empty($images)){
+                 if(Storage::exists($image->image)){
+                     Storage::delete($image->image);
+                 }
+                 $image->delete();
+             }
+              else{
+                  if(Storage::exists($image->image)){
+                      Storage::delete($image->image);
+                  }
+                  $image->delete();
+              }
+           }
+              return true;
+       } catch(\Exception $e){
+               return false;
+       }
+    }
+}
